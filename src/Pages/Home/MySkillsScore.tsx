a2903fbaf49587dfd6391e782ddf9a08534ecfb0
SanderVanryckeghem <sander.vanryckeghem@student.odisee.be>
2023-12-28 11:16:55 +0100

    fix styling mySkills

--- src/Pages/Home/MySkillsScore.tsx
+++ src/Pages/Home/MySkillsScore.tsx
@@ -1,17 +1,19 @@
-import data from '../../data/index.json'
-import CircularProgressBar from '../../Components/CircularProgressBar/CircularProgressBar'
+import data from "../../data/index.json";
+import CircularProgressBar from "../../Components/CircularProgressBar/CircularProgressBar";
 export default function MySkillsScore() {
-    return (
-        <section className="skills-score-section" id="mySkillsScore">
-            <h2 className="skills-section-heading">skills</h2>
-            <div className="skills-score-section-container">
-                {data.skillBar.map(skill => (
-                    <div className="skills-score-card" key={skill.type}>
-                        <h3 className="skills-score-section-title">{skill.type}</h3>
-                        <CircularProgressBar value={skill.level} url={skill.url} />
-                    </div>
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section className="skill-score-section" id="mySkillsScore">
+      <div className="portfolio-container">
+        <h2 className="skills-section-heading">My Skills</h2>
+      </div>
+      <div className="skills-score-section-container">
+        {data.skillBar.map((skill) => (
+          <div className="skills-score-card" key={skill.type}>
+            <h3 className="skills-score-section-title">{skill.type}</h3>
+            <CircularProgressBar value={skill.level} url={skill.url} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
