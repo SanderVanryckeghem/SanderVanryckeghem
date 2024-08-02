34629de64c958b175f5e28d15f7ddb18b172688c
SanderVanryckeghem <Sandervanryckeghem@outlook.com>
2024-08-02 13:58:33 +0200

    add possible extra colour and change order of chapters in navbar

--- src/Pages/Home/Navbar.tsx
+++ src/Pages/Home/Navbar.tsx
@@ -56,71 +56,71 @@ function Navbar() {
         <ul>
           <li>
             <Link
-              onClick={closeMenu}
-              activeClass="navbar-active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="heroSection"
-              className="navbar-content"
+                onClick={closeMenu}
+                activeClass="navbar-active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="heroSection"
+                className="navbar-content"
             >
               Home
             </Link>
           </li>
           <li>
             <Link
-              onClick={closeMenu}
-              activeClass="navbar-active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="AboutMe"
-              className="navbar-content"
+                onClick={closeMenu}
+                activeClass="navbar-active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="mySkillsScore"
+                className="navbar-content"
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link
+                onClick={closeMenu}
+                activeClass="navbar-active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="AboutMe"
+                className="navbar-content"
             >
               About Me
             </Link>
           </li>
           <li>
             <Link
-              onClick={closeMenu}
-              activeClass="navbar-active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="MyPortfolio"
-              className="navbar-content"
+                onClick={closeMenu}
+                activeClass="navbar-active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="MyPortfolio"
+                className="navbar-content"
             >
               Portfolio
-            </Link>
-          </li>
-          <li>
-            <Link
-              onClick={closeMenu}
-              activeClass="navbar-active-content"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="mySkillsScore"
-              className="navbar-content"
-            >
-              Skills
             </Link>
           </li>
         </ul>
       </div>
       <Link
-        onClick={closeMenu}
-        activeClass="navbar-active-content"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-        to="Contact"
-        className="btn btn-outline-primary"
+          onClick={closeMenu}
+          activeClass="navbar-active-content"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          to="Contact"
+          className="btn btn-outline-primary"
       >
         Contact Me
       </Link>
