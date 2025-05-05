19ab7295de434953ca433daafd9a67c5c3da678d
Sander Vanryckeghem <78733641+SanderVanryckeghem@users.noreply.github.com>
2025-05-05 16:04:40 +0200

    Update: footer

--- src/Pages/Home/Footer.tsx
+++ src/Pages/Home/Footer.tsx
@@ -1,73 +1,12 @@
-import { Link } from "react-scroll";
 import React from "react";
 
 function Footer() {
   return (
     <footer className="footer-container">
-      <div className="footer-link-container">
-        <div>
-          <img
-            className="footer-img"
-            src="./logo1.png"
-            alt="SanderVanryckeghemLogo"
-          />
-        </div>
-        <div className="footer-items">
-          <ul>
-            <li>
-              <Link
-                activeClass="navbar-active-content"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="heroSection"
-                className="text-md"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="navbar-active-content"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="MyPortfolio"
-                className="text-md"
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="navbar-active-content"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="AboutMe"
-                className="text-md"
-              >
-                About Me
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="navbar-active-content"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-                to="Contact"
-                className="text-md"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
+      <div className="footer-content-wrapper">
+        <p className="footer-content">
+          Made with 💖 by Sander inspired by Wout 🏸
+        </p>
         <div className="footer-social-icon">
           <ul>
             <li>
@@ -116,12 +55,6 @@ function Footer() {
             </li>
           </ul>
         </div>
-      </div>
-      <hr className="divider" />
-      <div className="footer-content-container">
-        <p className="footer-content">
-          Made with 💖 by Sander inspired by Wout 🏸
-        </p>
       </div>
     </footer>
   );
